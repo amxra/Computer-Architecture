5fb2494d18065def3bc3cfc510bc29f865214043
Amira Adediran <amira@Amiras-MBP.ipnxng.net>
2020-03-25 19:01:53 +0100

    implement branch tables

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,12 @@
 
 import sys
 
+ #create instructions for LDI, PRN, and HLT programs
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+MUL = 0b10100010
+
 class CPU:
     """Main CPU class."""
 
@@ -11,28 +17,70 @@ class CPU:
         self.ram = [0] * 256 
         self.reg = [0] * 8
         self.pc = 0
-       
+        self.halted = False
+        self.branchtable = {}
+        self.branchtable[HLT] = self.handle_hlt
+        self.branchtable[LDI] = self.handle_ldi
+        self.branchtable[PRN] = self.handle_prn
+        self.branchtable[MUL] = self.handle_mul
+        
 
-    def load(self):
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, mdr, mar):
+        self.ram[mar] = mdr
+        
+    
+    def load(self, filename):
         """Load a program into memory."""
 
-        address = 0 #pointer to currently executing instruction
+        
+    #     program = sys.argv[1]
 
-        # For now, we've just hardcoded a program:
+    #     address = 0 #pointer to currently executing instruction
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+    #     # For now, we've just hardcoded a program:
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+    #     # program = [
+    #     #     # From print8.ls8
+    #     #     0b10000010, # LDI R0,8
+    #     #     0b00000000,
+    #     #     0b00001000,
+    #     #     0b01000111, # PRN R0
+    #     #     0b00000000,
+    #     #     0b00000001, # HLT
+    #     # ]
+
+
+
+        try:
+            address = 0
+            #open file
+            with open(filename) as f:
+                #read the lines
+                for line in f:
+                    # split line before and after comment symbol
+                    comment_split = line.split("#")
+
+                    # extract our number
+                    num = comment_split[0].strip() # trim whitespace
+
+                    # ignore blank lines
+                    if num == '':
+                        continue 
+
+                    # convert our binary string to a number
+                    val = int(num, 2)
+
+                    # store val at address in memory
+                    self.ram_write(val, address)
+
+                    address += 1
+
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {filename} not found")
+            sys.exit(2)
 
 
     def alu(self, op, reg_a, reg_b):
@@ -41,6 +89,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b] 
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -64,50 +114,70 @@ class CPU:
 
         print()
 
-    def ram_read(self, MAR):
-        return self.ram[MAR]
+    def handle_ldi(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
 
-    def raw_write(self, MAR, MDR):
-        self.reg[MAR] = MDR
+        self.reg[operand_a] = operand_b
+
+    def handle_prn(self):
+        operand_a = self.ram_read(self.pc + 1)
+        print(self.reg[operand_a])
+
+    def handle_hlt(self):
+        self.halted = True
+
+    def handle_mul(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.alu("MUL", operand_a, operand_b)
 
     def run(self):
-        """Run the CPU."""
-        #create instructions for LDI, PRN, and HLT programs
-        LDI = 0b10000010
-        PRN = 0b01000111
-        HLT = 0b00000001
-
-        #set running to True
-        running = True
-        
-        #while cpu is running
-        while running:
-            #  set instruction register per step 3
+        while self.halted is False:
             IR = self.ram[self.pc]
+            val = IR
+            op_count = val >> 6
+            IR_length = op_count + 1
+            self.branchtable[IR]()
 
-            # set operand_a to pc+1 per step 3
-            operand_a = self.ram_read(self.pc + 1)
-            # set operand_b to pc+2 per step 3
-            operand_b = self.ram_read(self.pc + 2)
-
-            # if the instruction register is LDI
-            if IR == LDI:
-                #set register of operand_a to operand_b, jump 3 in PC (to PRN currently)
-                self.reg[operand_a] = operand_b
-                self.pc +=3
-            # if the instruction register is PRN
-            elif IR == PRN:
-                #print the register of operand_a, jump 2 in PC
-                print(self.reg[operand_a])
-                self.pc +=2
-            # if the instruction register is the halt command
-            elif IR == HLT:
-                #set running to false and exit
-                running = False
-                sys.exit(0)
-            # if anything else, invalid command and quit with failure code 1
-            else:
-                print(f"Invalid Command: {self.ram[self.pc]}")
+            if IR == 0 or None:
+                print(f"Unknown instructions and index {self.pc}")
                 sys.exit(1)
+            self.pc += IR_length    
 
-    
+ 
+    # def run(self):
+    #     """Run the CPU."""
+       
+    #     #set running to True
+    #     running = True
+        
+    #     #while cpu is running
+    #     while running:
+    #         #  set instruction register per step 3
+    #         IR = self.ram[self.pc]
+
+    #         # set operand_a to pc+1 per step 3
+    #         operand_a = self.ram_read(self.pc + 1)
+    #         # set operand_b to pc+2 per step 3
+    #         operand_b = self.ram_read(self.pc + 2)
+
+    #         # if the instruction register is LDI
+    #         if IR == LDI:
+    #             #set register of operand_a to operand_b, jump 3 in PC (to PRN currently)
+    #             self.reg[operand_a] = operand_b
+    #             self.pc +=3
+    #         # if the instruction register is PRN
+    #         elif IR == PRN:
+    #             #print the register of operand_a, jump 2 in PC
+    #             print(self.reg[operand_a])
+    #             self.pc +=2
+    #         # if the instruction register is the halt command
+    #         elif IR == HLT:
+    #             #set running to false and exit
+    #             running = False
+    #             sys.exit(0)
+    #         # if anything else, invalid command and quit with failure code 1
+    #         else:
+    #             print(f"Invalid Command: {self.ram[self.pc]}")
+    #             sys.exit(1)
